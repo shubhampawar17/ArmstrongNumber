84935c0dcdf0ac4a327b5bfeb4c4f7a2b77907a5
shubhampawar17 <sp1180253@gmail.com>
2024-09-25 14:17:06 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArmstrongNumber.model;
+
+
+
+namespace ArmstrongNumber
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+            int num , choice;
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine("Enter a Number:");
+                
+
+                while (true)
+                {
+                    num = int.Parse(Console.ReadLine());
+                    if (num >= 0)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Please enter a valid positive number:");
+                }
+
+                string result = Armstrong.IsArmstrong(num) ? "Armstrong Number" : "Not an Armstrong Number";
+                Console.WriteLine(result);
+
+                Console.WriteLine("\nChoose an option:");
+                Console.WriteLine("1. Check another number");
+                Console.WriteLine("2. Exit");
+
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+
+            Console.WriteLine("Program exited.");
+        }
+    }
+}
+
+    
+
---
+++ model/Armstrong.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmstrongNumber.model
+{
+    internal class Armstrong
+    {
+        public static bool IsArmstrong(int num)
+        {
+            int sum = 0, temp = num, rem;
+            int digits = num.ToString().Length;
+
+            while (num > 0)
+            {
+                rem = num % 10;
+                sum = sum + (int)Math.Pow(rem, digits);
+                num = num + 10;
+            }
+
+            return temp == sum;
+        }
+    }
+}
